bb693bc0198e4a3eaea9be4484c34a38ba9f873e
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2025-12-19 14:03:56 +0545

    control flow using if else

---
+++ projects/03-component-composition/src/app/userinfo/userinfo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-userinfo',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <p>userinfo works here in the userComponents!</p> `,
+  styles: ``,
+})
+export class UserinfoComponent {}
